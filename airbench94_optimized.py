d48f8d31fa8f14391438622d6c9409c21a1d2fd6
clarenochieng <98944975+clarenochieng@users.noreply.github.com>
2025-12-09 00:46:39 -0500

    Adjust learning rate and weight decay values

--- airbench94_optimized.py
+++ airbench94_optimized.py
@@ -40,9 +40,9 @@ hyp = {
     'opt': {
         'train_epochs': 9.9,
         'batch_size': 1024,
-        'lr': 11.5,                 # learning rate per 1024 examples
+        'lr': 10,                 # learning rate per 1024 examples
         'momentum': 0.85,
-        'weight_decay': 0.0153,     # weight decay per 1024 examples (decoupled from learning rate)
+        'weight_decay': 0.015,     # weight decay per 1024 examples (decoupled from learning rate)
         'bias_scaler': 64.0,        # scales up learning rate (but not weight decay) for BatchNorm biases
         'label_smoothing': 0.2,
         'whiten_bias_epochs': 3,    # how many epochs to train the whitening layer bias before freezing
